c6c195ad12117af840edec289ff81a5f4109bdaf
Tomek586 <tomasz.dziedzic@microsoft.wsei.edu.pl>
2023-05-27 12:19:05 +0200

    metoda login_btt_click oraz register_btt_click

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,7 +36,48 @@ namespace BookingApp
         }
         private void Login_btt_click()
         {
+            try
+            {
+                string FileName = "ConcertsBookings.mdf";
+                string CurrentDirectory = Directory.GetCurrentDirectory();
+                string ProjectDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(CurrentDirectory).FullName).FullName).FullName;
+                string FilePath = Path.Combine(ProjectDirectory, FileName);
 
+                string conn = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={FilePath};Integrated Security=True;Connect Timeout=30;";
+                SqlConnection con = new SqlConnection(conn);
+
+
+                con.Open();
+                string add_data = "Select * from [dbo].[logins] where username=@username and password=@password ";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+
+
+                cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Password);
+                cmd.ExecuteNonQuery();
+                int Count = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+
+                txtUsername.Text = "";
+                txtPassword.Password = "";
+                if (Count > 0)
+                {
+                    UserPanel u1 = new UserPanel();
+                    this.Close();
+                    u1.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("chujoza");
+                }
+
+
+            }
+            catch
+            {
+
+            }
         }
 
         private void Button_click(object sender, RoutedEventArgs e)
--- Register.xaml.cs
+++ Register.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace BookingApp
 {
@@ -23,9 +26,39 @@ namespace BookingApp
         {
             InitializeComponent();
         }
-        private void Register_btt_click()
+        private void Register_btt_click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                string FileName = "ConcertsBookings.mdf";
+                string CurrentDirectory = Directory.GetCurrentDirectory();
+                string ProjectDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(CurrentDirectory).FullName).FullName).FullName;
+                string FilePath = Path.Combine(ProjectDirectory, FileName);
 
+                string conn = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={FilePath};Integrated Security=True;Connect Timeout=30;";
+                SqlConnection con = new SqlConnection(conn);
+
+
+                con.Open();
+                string add_data = "INSERT into [dbo].[logins] values('',@username, @password) ";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+
+
+                cmd.Parameters.AddWithValue("@username", username.Text);
+                cmd.Parameters.AddWithValue("@password", password.Password);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                username.Text = "";
+                password.Password = "";
+
+                MainWindow w1 = new MainWindow();
+                this.Close();
+                w1.Show();
+            }
+            catch
+            {
+
+            }
         }
     }
 }
